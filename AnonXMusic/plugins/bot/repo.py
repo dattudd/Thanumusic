56cd55f96a43d17db6c32832426fdba5bc5b7664
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-06-09 11:44:10 +0530

    Update repo.py

--- AnonXMusic/plugins/bot/repo.py
+++ AnonXMusic/plugins/bot/repo.py
@@ -26,7 +26,7 @@ async def start(_, msg):
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://telegra.ph/file/9e170bd102c6198c4cfb9.jpg",
+        photo="https://telegra.ph/file/a950dba51f34f48ddbf51.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
