f5d644ea93c6db09e6ac030b308f1ad4bba7cd13
GDN Bharath <148096435+dattudd@users.noreply.github.com>
2024-06-13 22:35:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ API_ID = int(getenv("API_ID", "20798317"))
 API_HASH = getenv("API_HASH", "82a94d416e05ca5cc3bc04da8494d7ca")
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "6829228356:AAEB7cJaVxj4YHIaaYkW3K1g9Bs95KUpVEc")
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
