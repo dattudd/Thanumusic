7d5316c22e2fc13057a854e79ab823104744d28e
GDN Bharath <dn6418138@gmail.com>
2024-08-20 16:27:37 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,8 +1,21 @@
-!/bin/bash
-echo "Today is " `date`
+/usr/bin/env bash
 
-echo -e "\nenter the path to directory"
-read the_path
-
-echo -e "\n you path has the following files and folders: "
-ls $the_path
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET=$(readlink "$SOURCE")
+  if [[ $TARGET == /* ]]; then
+    echo "SOURCE '$SOURCE' is an absolute symlink to '$TARGET'"
+    SOURCE=$TARGET
+  else
+    DIR=$( dirname "$SOURCE" )
+    echo "SOURCE '$SOURCE' is a relative symlink to '$TARGET' (relative to '$DIR')"
+    SOURCE=$DIR/$TARGET # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  fi
+done
+echo "SOURCE is '$SOURCE'"
+RDIR=$( dirname "$SOURCE" )
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+if [ "$DIR" != "$RDIR" ]; then
+  echo "DIR '$RDIR' resolves to '$DIR'"
+fi
+echo "DIR is '$DIR'"
